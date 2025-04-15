a446d5b4555273ec75d32db2cc5bdc80e58b3be9
Pascal Noser <noser.pascal95@gmail.com>
2025-04-15 09:58:30 +0200

    Updated navigate_to_active_file to also open the Files pane

--- R/misc.R
+++ R/misc.R
@@ -1,8 +1,12 @@
-#' In Files pane, navigate to folder of file currently open in editor
+#' Navigate to directory of open file
+#'
+#' Open the Files pane and navigate to the folder containing the file currently
+#' open in the editor.
 #'
 #' @export
 #'
 navigate_to_active_file <- function() {
   open_file <- rstudioapi::getSourceEditorContext()$path
   rstudioapi::filesPaneNavigate(open_file)
+  rstudioapi::executeCommand("activateFiles")
 }
